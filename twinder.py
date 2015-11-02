9d6ea70f8d4c97899218437304263abfbcca3398
pinae <jonny@pinae.net>
2015-11-02 21:12:45 +0100

    Reading the timeline

--- twinder.py
+++ twinder.py
@@ -12,3 +12,7 @@ if not os.path.exists(MY_TWITTER_CREDENTIALS):
 oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDENTIALS)
 
 twitter = Twitter(auth=OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+
+timeline = twitter.statuses.home_timeline()
+for tweet in timeline:
+    print(tweet['text'])
